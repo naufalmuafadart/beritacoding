92d0ae83b9eead2efdada4c5d3dbe55aadc63f4a
naufalmuafadart <mnaufalmuafa.dart@gmail.com>
2023-08-27 15:16:08 +0700

    Learn controller

--- application/controllers/Page.php
+++ application/controllers/Page.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Page extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -20,6 +20,14 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('home');
+	}
+
+	public function about() {
+		$this->load->view('about');
+	}
+
+	public function contact() {
+		$this->load->view('contact');
 	}
 }
